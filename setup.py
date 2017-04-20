2302d7b22f803f7476a7f7b4f123bf3bd55360e4
Jesse Myers <jesse.myers@globality.com>
2017-04-19 20:26:59 -0700

    Bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages, setup
 
 project = "microcosm_pubsub"
-version = "0.27.2"
+version = "0.28.0"
 
 setup(
     name=project,
@@ -18,7 +18,7 @@ setup(
         "boto3>=1.3.0",
         "marshmallow>=2.12.1",
         "microcosm>=0.17.2",
-        "microcosm-daemon>=0.10.0",
+        "microcosm-daemon>=0.11.0",
         "microcosm-logging>=0.12.0",
     ],
     setup_requires=[
