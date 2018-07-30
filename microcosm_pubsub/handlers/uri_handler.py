27045bcdf21f13fa5d91b7c43c265a356f4d5921
pelegdvir <gaspipe@gmail.com>
2018-07-30 11:09:52 -0700

    URIHandler.resource_type: use isinstance (#94)

--- microcosm_pubsub/handlers/uri_handler.py
+++ microcosm_pubsub/handlers/uri_handler.py
@@ -123,7 +123,7 @@ class URIHandler:
             raise Nack(self.nack_timeout)
         response.raise_for_status()
         body = response.json()
-        if type(body) is self.resource_type:
+        if isinstance(body, self.resource_type):
             return body
         return self.resource_type(**body)
 
