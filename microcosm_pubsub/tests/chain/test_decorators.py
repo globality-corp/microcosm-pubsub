5d6728951c6cf89bbecbd40c7c4420eb992fdd3d
pelegdvir <gaspipe@gmail.com>
2018-08-06 08:51:31 -0700

    Advanced context decorators (#100)

--- microcosm_pubsub/tests/chain/test_decorators.py
+++ microcosm_pubsub/tests/chain/test_decorators.py
@@ -18,9 +18,25 @@ from microcosm_pubsub.chain.context_decorators import (
 class TestDecorators:
 
     def test_get_from_context(self):
-        context = dict(arg=123)
+        context = dict(arg=200)
         wrapped = get_from_context(context, lambda arg: arg)
-        assert_that(wrapped(), is_(123))
+        assert_that(wrapped(), is_(200))
+
+    def test_get_from_context_default_value(self):
+        def function(a, b=10):
+            return a + b
+
+        context = dict()
+        wrapped = get_from_context(context, function)
+        assert_that(calling(wrapped), raises(KeyError))
+
+        context = dict(a=190)
+        wrapped = get_from_context(context, function)
+        assert_that(wrapped(), is_(200))
+
+        context = dict(a=190, b=11)
+        wrapped = get_from_context(context, function)
+        assert_that(wrapped(), is_(201))
 
     def test_save_to_context(self):
         context = dict()
@@ -40,12 +56,46 @@ class TestDecorators:
         def extract_args(num1, num2):
             return num1, num2
 
+        @extracts("from_callable")
+        class CallableExtractor:
+            def __call__(self, number):
+                return number
+
+        class Extractor:
+            def method(self, number):
+                return number
+
+            @classmethod
+            def class_method(cls, number):
+                return number
+
+            @staticmethod
+            def static_method(number):
+                return number
+
+        wrapped_method = extracts("from_method")(Extractor().method)
+        wrapped_class_method = extracts("from_class_method")(Extractor().class_method)
+        wrapped_static_method = extracts("from_static_method")(Extractor().static_method)
+
         save_to_context(context, dont_extract)(-1)
         save_to_context(context, extract_arg)(0)
         save_to_context(context, extract_arg1_and_arg2)(1, 2)
         save_to_context(context, extract_args)(1, 2)
+        save_to_context(context, CallableExtractor())(3)
+        save_to_context(context, wrapped_method)(4)
+        save_to_context(context, wrapped_class_method)(5)
+        save_to_context(context, wrapped_static_method)(6)
 
-        assert_that(context, is_(equal_to(dict(arg=0, arg1=1, arg2=2, args=(1, 2)))))
+        assert_that(context, is_(equal_to(dict(
+            arg=0,
+            arg1=1,
+            arg2=2,
+            args=(1, 2),
+            from_callable=3,
+            from_method=4,
+            from_class_method=5,
+            from_static_method=6,
+        ))))
 
     def test_overriding_extracts(self):
         context = dict()
@@ -129,7 +179,7 @@ class TestDecorators:
 
         assert_that(wrapped(), is_(123))
 
-    def test_temporarily_replace_context_keys_wring_order(self):
+    def test_temporarily_replace_context_keys_wrong_order(self):
         context = dict(arg=123)
 
         func = binds(arg="param")(lambda param: param)
@@ -137,7 +187,7 @@ class TestDecorators:
         wrapped = temporarily_replace_context_keys(context, func)
         wrapped = get_from_context(context, wrapped)
 
-        assert_that(calling(wrapped), raises(TypeError))
+        assert_that(calling(wrapped), raises(KeyError))
 
     def test_temporarily_replace_missing_keys(self):
         context = dict()
@@ -148,3 +198,24 @@ class TestDecorators:
         wrapped = temporarily_replace_context_keys(context, wrapped)
 
         assert_that(calling(wrapped), raises(KeyError))
+
+    def test_multiple_decorators(self):
+        @extracts("res_obj")
+        class Extractor:
+            def __call__(self, arg):
+                return arg
+
+        @extracts("res_func")
+        def extractor(arg):
+            return arg
+
+        context = dict(arg=123)
+        wrapped_object = save_to_context(context, get_from_context(context, Extractor()))
+        wrapped_function = save_to_context(context, get_from_context(context, extractor))
+        wrapped_object()
+        wrapped_function()
+        assert_that(context, is_(equal_to(dict(
+            arg=123,
+            res_obj=123,
+            res_func=123,
+        ))))
