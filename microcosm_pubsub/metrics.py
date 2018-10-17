87c888837b8145a3e98e99d416c532a2424ac1f2
Moshe Zadka <moshe.zadka@globality.com>
2018-10-17 09:51:56 -0700

    fix import

---
+++ microcosm_pubsub/metrics.py
@@ -0,0 +1,36 @@
+from distutils.util import strtobool
+
+from microcosm.api import defaults
+from microcosm_pubsub.result import MessageHandlingResultType
+
+def do_nothing(results):
+    return
+
+@defaults(
+    enable_metrics="false",
+)
+def configure_pubsub_metrics(graph):
+    enable_metrics = strtobool(graph.config.route.enable_metrics)
+    if not enable_metrics:
+        return do_nothing
+    metrics = graph.metrics
+    tags = ["source:micrcocosm-pubsub"]
+    
+    def send_metrics(results):
+        if results.result == MessageHandlingResultType.IGNORED:
+            return
+        extra_tags = [
+            f"result:{results.result}",
+            f"media-type:{results.media_type}",
+        ]
+        graph.metrics.increment(
+            "message_count",
+            tags=tags + extra_tags,
+        )
+        graph.metrics.histogram(
+            "message",
+            results.elapsed_time,
+            tags=tags + extra_tags,
+        )
+
+    return send_metrics
