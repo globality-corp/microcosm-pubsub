23de346c784535662af16a6edba5b18af73f6dad
Moshe Zadka <moshe.zadka@globality.com>
2018-10-17 14:40:59 -0700

    refactor to avoid closures

--- microcosm_pubsub/metrics.py
+++ microcosm_pubsub/metrics.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 from microcosm.api import defaults, typed
 from microcosm.config.types import boolean
 from microcosm_pubsub.result import MessageHandlingResultType
@@ -7,6 +9,21 @@ def do_nothing(results):
     return
 
 
+def send_metrics(metrics, results):
+    if results.result == MessageHandlingResultType.IGNORED:
+        return
+    tags = [
+        "source:micrcocosm-pubsub",
+        f"result:{results.result}",
+        f"media-type:{results.media_type}",
+    ]
+    metrics.histogram(
+        "message",
+        results.elapsed_time,
+        tags=tags,
+    )
+
+
 @defaults(
     enable=typed(boolean, default_value=False)
 )
@@ -14,20 +31,5 @@ def configure_pubsub_metrics(graph):
     enable_metrics = graph.config.pubsub_send_metrics.enable
     if not enable_metrics:
         return do_nothing
-    graph.use("metrics")
-    tags = ["source:micrcocosm-pubsub"]
-
-    def send_metrics(results):
-        if results.result == MessageHandlingResultType.IGNORED:
-            return
-        extra_tags = [
-            f"result:{results.result}",
-            f"media-type:{results.media_type}",
-        ]
-        graph.metrics.histogram(
-            "message",
-            results.elapsed_time,
-            tags=tags + extra_tags,
-        )
-
-    return send_metrics
+    else:
+        return partial(send_metrics, graph.metrics)
