e586747f8bf96bee0dabf379be377c73f2253999
Saivickna Raveendran <saivickna.r@gmail.com>
2019-02-07 16:26:53 -0800

    Add message reason to extra dictionary (#149)
    Why?
    
    When we have multiple SkipMessages and provide a reason, it will not show up in the log.
    
    * Fix imports
    * Update isort

--- microcosm_pubsub/tests/test_result.py
+++ microcosm_pubsub/tests/test_result.py
@@ -2,10 +2,7 @@
 Test result handling.
 
 """
-from hamcrest import (
-    assert_that,
-    has_properties,
-)
+from hamcrest import assert_that, has_entries, has_properties
 from microcosm_logging.decorators import logger
 
 from microcosm_pubsub.errors import IgnoreMessage, Nack, SkipMessage, TTLExpired
@@ -91,6 +88,9 @@ class TestMessageHandlingResult:
             has_properties(
                 media_type="application/vnd.microcosm.derived",
                 result=MessageHandlingResultType.SKIPPED,
+                extra=has_entries(
+                    reason="ignorance is bliss",
+                ),
             ),
         )
         # ack
