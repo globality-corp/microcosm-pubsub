3967af9405172261754677ca8d890179b2f59449
Adrien Fallou <adrien.fallou@gmail.com>
2019-06-27 20:57:45 +0100

    Export  in library

--- microcosm_pubsub/chain/statements/__init__.py
+++ microcosm_pubsub/chain/statements/__init__.py
@@ -4,6 +4,7 @@ from microcosm_pubsub.chain.statements.assign import (  # noqa: F401
     assign_function,
     extract,
 )
+from microcosm_pubsub.chain.statements.call import call  # noqa: F401
 from microcosm_pubsub.chain.statements.for_each import for_each  # noqa: F401
 from microcosm_pubsub.chain.statements.switch import switch  # noqa: F401
 from microcosm_pubsub.chain.statements.try_chain import try_chain  # noqa: F401
