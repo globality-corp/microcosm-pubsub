ab7a8748b0463f649ec0575bc848194c433f5b78
Avi Zurel <avi@kensodev.com>
2019-08-02 17:48:32 -0700

    bumping microcosm version [ci skip]

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
         "boto3>=1.5.8",
         "dataclasses;python_version<'3.7'",
         "marshmallow>=2.15.0",
-        "microcosm>=2.0.0",
+        "microcosm>=2.12.0",
         "microcosm-daemon>=1.0.0",
         "microcosm-logging>=1.3.0",
     ],
