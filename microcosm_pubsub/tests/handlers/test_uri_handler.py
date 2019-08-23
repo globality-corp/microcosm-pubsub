1bc160242fb7d3c458f7b512665328300d29301d
Adam Ever-Hadani <adamhadani@globality.com>
2019-08-23 13:43:42 -0700

    WIP
    unit-tests for caching
    
    resource cache file
    
    make pymemcache an optional dependency of .[caching]
    
    flake8 fixes
    
    add parameterized to entrypoint.sh for testing
    
    flake8-isort fixes
    
    remove tox, add separate test entrypoints for caching extras and no caching
    
    fix test entrypoint, disable cache by default
    
    support per-instance disabling of resource cache
    
    support whitelisting of resource cache

--- microcosm_pubsub/tests/handlers/test_uri_handler.py
+++ microcosm_pubsub/tests/handlers/test_uri_handler.py
@@ -10,6 +10,8 @@ from hamcrest import (
     raises,
 )
 from microcosm.api import create_object_graph
+from microcosm.loaders import load_from_dict
+from nose.plugins.attrib import attr
 
 from microcosm_pubsub.errors import Nack
 from microcosm_pubsub.handlers import URIHandler
@@ -104,6 +106,173 @@ class TestURIHandler:
             resource.foo,
             is_(equal_to("bar")),
         )
+
+    @attr("caching")
+    def test_get_resource_with_cache_disabled_on_instance(self):
+        config = dict(
+            resource_cache=dict(
+                enabled=True,
+            ),
+        )
+        graph = create_object_graph("microcosm", testing=True, loader=load_from_dict(config))
+        graph.use(
+            "opaque",
+            "resource_cache",
+            "sqs_message_context",
+        )
+        graph.lock()
+
+        uri = "http://localhost"
+        message = dict(
+            uri=uri,
+        )
+        json_data = dict(foo="bar", bar="baz")
+
+        with patch("microcosm_pubsub.handlers.uri_handler.get") as mocked_get:
+            mocked_get.return_value = MockResponse(
+                status_code=200,
+                json_data=json_data,
+            )
+            handler = URIHandler(graph, resource_cache_enabled=False)
+            handler.get_resource(message, uri)
+
+        assert_that(handler.resource_cache, is_(None))
+
+        mocked_get.assert_called_with(
+            uri,
+            headers=dict(),
+        )
+
+    @attr("caching")
+    def test_get_non_whitelisted_resource_with_cache_enabled(self):
+        config = dict(
+            resource_cache=dict(
+                enabled=True,
+            ),
+        )
+        graph = create_object_graph("microcosm", testing=True, loader=load_from_dict(config))
+        graph.use(
+            "opaque",
+            "resource_cache",
+            "sqs_message_context",
+        )
+        graph.lock()
+
+        uri = "http://localhost"
+        message = dict(
+            uri=uri,
+        )
+        json_data = dict(foo="bar", bar="baz")
+
+        with patch("microcosm_pubsub.handlers.uri_handler.get") as mocked_get:
+            mocked_get.return_value = MockResponse(
+                status_code=200,
+                json_data=json_data,
+            )
+            handler = URIHandler(graph)
+            with patch.object(handler.resource_cache, "get") as mocked_cache_get:
+                mocked_cache_get.return_value = None
+                with patch.object(handler.resource_cache, "set") as mocked_cache_set:
+                    handler.get_resource(message, uri)
+
+        mocked_get.assert_called_with(
+            uri,
+            headers=dict(),
+        )
+
+        # Nb. cache get/set were not called due to resource uri not being whitelisted
+        assert_that(mocked_cache_get.called, is_(equal_to(False)))
+        assert_that(mocked_cache_set.called, is_(equal_to(False)))
+
+    @attr("caching")
+    def test_get_whitelisted_resource_with_cache_enabled_and_cache_miss(self):
+        config = dict(
+            resource_cache=dict(
+                enabled=True,
+            ),
+        )
+        graph = create_object_graph("microcosm", testing=True, loader=load_from_dict(config))
+        graph.use(
+            "opaque",
+            "resource_cache",
+            "sqs_message_context",
+        )
+        graph.lock()
+
+        uri = "https://service.env.globality.io/api/v2/project_event/0598355c-5b19-49bd-a755-146204220a5b"
+        message = dict(
+            uri=uri,
+        )
+        json_data = dict(foo="bar", bar="baz")
+
+        with patch("microcosm_pubsub.handlers.uri_handler.get") as mocked_get:
+            mocked_get.return_value = MockResponse(
+                status_code=200,
+                json_data=json_data,
+            )
+            handler = URIHandler(graph)
+
+            with patch.object(handler.resource_cache, "get") as mocked_cache_get:
+                mocked_cache_get.return_value = None
+                with patch.object(handler.resource_cache, "set") as mocked_cache_set:
+                    handler.get_resource(message, uri)
+
+        mocked_get.assert_called_with(
+            uri,
+            headers=dict(),
+        )
+        # Nb. cache get was attempted
+        mocked_cache_get.assert_called_with(
+            uri,
+        )
+        # Nb. cache set was called due to cache miss
+        mocked_cache_set.assert_called_with(
+            uri,
+            json_data,
+        )
+
+    @attr("caching")
+    def test_get_whitelisted_resource_with_cache_enabled_and_cache_hit(self):
+        config = dict(
+            resource_cache=dict(
+                enabled=True,
+            ),
+        )
+        graph = create_object_graph("microcosm", testing=True, loader=load_from_dict(config))
+        graph.use(
+            "opaque",
+            "resource_cache",
+            "sqs_message_context",
+        )
+        graph.lock()
+
+        uri = "https://service.env.globality.io/api/v2/project_event/0598355c-5b19-49bd-a755-146204220a5b"
+        message = dict(
+            uri=uri,
+        )
+        json_data = dict(foo="bar", bar="baz")
+
+        with patch("microcosm_pubsub.handlers.uri_handler.get") as mocked_get:
+            mocked_get.return_value = MockResponse(
+                status_code=200,
+                json_data=json_data,
+            )
+            handler = URIHandler(graph)
+
+            with patch.object(handler.resource_cache, "get") as mocked_cache_get:
+                mocked_cache_get.return_value = json_data
+                with patch.object(handler.resource_cache, "set") as mocked_cache_set:
+                    handler.get_resource(message, uri)
+
+        # Nb. cache get was attempted
+        mocked_cache_get.assert_called_with(
+            uri,
+        )
+        # Nb. actual resource HTTP retrieve not called since cache was hit
+        assert_that(mocked_get.called, is_(equal_to(False)))
+
+        # Nb. cache set was not called due to cache hit
+        assert_that(mocked_cache_set.called, is_(equal_to(False)))
 
     def test_get_resource_empty_headers(self):
         graph = create_object_graph("microcosm")
